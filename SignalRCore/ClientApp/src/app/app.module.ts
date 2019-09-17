2ff38bfe644dd343f4489e8f978db46dced87c84
Ivan Rubi <rubilnik4@yandex.ru>
2019-09-18 01:11:09 +0600

    Add j2t
    Add j2t

--- SignalRCore/ClientApp/src/app/app.module.ts
+++ SignalRCore/ClientApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RequestComponent } from './request/request.component';
